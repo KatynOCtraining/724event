89e57200319c3f565ebe70b49b2b85dfb4f1bf6f
KatynOCtraining <t.theveupolaris@gmail.com>
2024-02-06 03:38:40 +0100

    slider fix

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,57 +1,66 @@
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
-
 import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
-  };
+  const [sortedData, setSortedData] = useState([]);
+
+  // Tri des événements du plus ancien au plus récent au moment du chargement du composant
   useEffect(() => {
-    nextCard();
-  });
+    if (data?.focus) {
+      const sorted = [...data.focus].sort(
+        (a, b) => new Date(a.date) - new Date(b.date)
+      );
+      setSortedData(sorted);
+    }
+  }, [data]);
+
+  // Gestion de l'index pour le slider
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % sortedData.length);
+    }, 5000);
+
+    // Nettoyer le timer lors du démontage du composant
+    return () => clearTimeout(timer);
+  }, [index, sortedData.length]);
+
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+      {sortedData.map((event, idx) => (
+        <div
+          key={event.id}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt={event.title} />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {sortedData.map((event, radioIdx) => (
+            <input
+              key={event.id}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => setIndex(radioIdx)}
+              readOnly
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
