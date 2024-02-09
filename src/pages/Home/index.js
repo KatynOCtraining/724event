d98947f25a5bf5a80c0ed46f74d27dd8f19fe1b8
KatynOCtraining <t.theveupolaris@gmail.com>
2024-02-09 01:41:30 +0100

    fix services

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -35,18 +35,29 @@ const Page = () => {
             <ServiceCard
               imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png"
               title="Soirée d’entreprise"
-              description="Une soirée d’entreprise vous permet de réunir vos équipes pour un moment convivial afin de valoriser votre société en projetant une image dynamique. Nous vous proposons d’organiser pour vous vos diners et soirée d’entreprise."
-            />
-            <ServiceCard
-              imageSrc="/images/hall-expo.png"
-              title="Conférences"
-              description="724 events vous propose d’organiser votre évènement, quelle que soit sa taille, en s’adaptant à votre demande et à vos demandes. En tant que spécialistes de l’évènementiel, nous saurons trouver le lieu parfait ainsi que des solutions inédites pour capter votre audience et faire de cet évènement un succès."
-            />
+            >
+              Une soirée d’entreprise vous permet de réunir vos équipes pour un
+              moment convivial afin de valoriser votre société en projetant une
+              image dynamique. Nous vous proposons d’organiser pour vous vos
+              diners et soirée d’entreprise.
+            </ServiceCard>
+            <ServiceCard imageSrc="/images/hall-expo.png" title="Conférences">
+              724 events vous propose d’organiser votre évènement, quelle que
+              soit sa taille, en s’adaptant à votre demande et à vos demandes.
+              En tant que spécialistes de l’évènementiel, nous saurons trouver
+              le lieu parfait ainsi que des solutions inédites pour capter votre
+              audience et faire de cet évènement un succès.
+            </ServiceCard>
             <ServiceCard
               imageSrc="/images/sophia-sideri-LFXMtUuAKK8-unsplash1.png"
               title="Experience digitale"
-              description="Notre agence experte en contenus immersifs offre des services de conseil aux entreprises, pour l’utilisation de la réalité virtuelle, de la réalité augmentée et de la réalité mixte de l’animation événementielle, à la veille technologique jusqu’au développement de module de formation innovant."
-            />
+            >
+              Notre agence experte en contenus immersifs offre des services de
+              conseil aux entreprises, pour l’utilisation de la réalité
+              virtuelle, de la réalité augmentée et de la réalité mixte de
+              l’animation événementielle, à la veille technologique jusqu’au
+              développement de module de formation innovant.
+            </ServiceCard>
           </div>
         </section>
         <section className="EventsContainer">
